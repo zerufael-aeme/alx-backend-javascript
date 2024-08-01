fb5bbabbff2715413439a2c25b4bdb2088aa2bd3
zerufael <zerufaelaemero@gmail.com>
2024-08-01 07:25:59 -0700

    ES6

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,14 @@
+export function taskFirst() {
+    let task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    const combination = 'But sometimes let' + getLast();
+  
+    return combination;
+  }
