4613b89357278fe44aa64a3718bb6028ff68e7cd
zerufael <zerufaelaemero@gmail.com>
2024-08-09 12:06:42 -0700

    ObjReport

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  const all = {
+    allEmployees: {
+      ...employeesList
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return all;
+}
