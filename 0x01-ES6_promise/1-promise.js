341ec07fc68d360295e82b505b8ae686559be4f6
zerufael <zerufaelaemero@gmail.com>
2024-08-15 12:54:03 -0700

    edit

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,8 @@
 export default function getResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-
-        if (success)
-            resolve({status: 200, body: 'Success'});
-        else
-            reject("The fake API is not working currently");
-    });
+  return new Promise((resolve, reject) => {
+    if (success)
+      resolve({status: 200, body: 'Success'});
+    else if(!success)
+      reject("The fake API is not working currently");
+  });
 }
