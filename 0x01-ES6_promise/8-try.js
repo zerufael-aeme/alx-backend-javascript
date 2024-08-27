02a4b0a5bc04d4e4f68bd681c6b3e11ec9381593
zerufael <zerufaelaemero@gmail.com>
2024-08-27 07:31:34 -0700

    edit

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,9 @@
 export default function divideFunction(numerator, denominator) {
   try{
     if (denominator === 0)
-      throw new Error('cannot divide by 0');
+      throw new('cannot divide by 0');
     return numerator / denominator
   }catch (error) {
-    console.error('Error:', error.message);
+    console.error(error.message);
   }
 }
