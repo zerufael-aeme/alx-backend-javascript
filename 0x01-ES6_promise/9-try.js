617f88db6fbcf96da32895f7560706b6d39223da
zerufael <zerufaelaemero@gmail.com>
2024-08-27 08:07:38 -0700

    edit

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,9 +2,9 @@ export default function guardrail(mathFunction) {
   const queue = [];
   queue[1] = 'Guardrail was processed';
   try {
-    queue[0] = mathFunction();   
-  }catch (error){
-    queue[0] = 'Error: ' + error.message;
+    queue[0] = mathFunction();
+  } catch (error) {
+    queue[0] = `Error: ${error.message}`;
   }
   return queue;
 }
