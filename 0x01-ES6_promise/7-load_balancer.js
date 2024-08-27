fc80cda3e35cd24c86a1ff5ab3e5d1a06a379664
zerufael <zerufaelaemero@gmail.com>
2024-08-27 06:29:43 -0700

    edit

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
-  .race([chinaDownload(), USDownload()])
+  .race([chinaDownload, USDownload])
   .then((value) => value)
 }
