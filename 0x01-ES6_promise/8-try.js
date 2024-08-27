f9064facc985c0d475e49be830353f9cd4ddd152
zerufael <zerufaelaemero@gmail.com>
2024-08-27 07:20:50 -0700

    try

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+export default function divideFunction(numerator, denominator) {
+  try{
+    if (denominator === 0)
+      throw new Error('cannot divide by 0');
+    return numerator / denominator
+  }catch (error) {
+    console.error('Error:', error.message);
+  }
+}
