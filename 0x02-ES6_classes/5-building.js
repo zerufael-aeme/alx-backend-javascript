9612fe6a4ac8c30c93c7ccb893eca17b5a872c58
zerufael <zerufaelaemero@gmail.com>
2024-09-01 13:01:00 -0700

    edit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -8,6 +8,8 @@ export default class Building {
   }
 
   evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
+    if (this.constructor === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
