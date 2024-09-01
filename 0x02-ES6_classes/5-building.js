af7def2fb04f2f99d81b55a6e7e4e683fc8f97af
zerufael <zerufaelaemero@gmail.com>
2024-09-01 13:15:40 -0700

    edit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (new.target === Building) {
-      throw new Error("Cannot instantiate an abstract class.");
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
