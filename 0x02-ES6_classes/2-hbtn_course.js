e089175ff58edaed850c36220ddc50e840fe8986
zerufael <zerufaelaemero@gmail.com>
2024-09-01 06:59:05 -0700

    edit

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,12 +2,18 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
+    } else {
+      throw new Error('Name must be a string');
     }
     if (typeof length === 'number') {
       this._length = length;
+    } else {
+      throw new Error('Length must be a number');
     }
     if (Array.isArray(students)) {
       this._students = students;
+    } else {
+      throw new Error('Students must be an array');
     }
   }
 
@@ -16,7 +22,11 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    return this._name = name;
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Name must be a string');
+    }
   }
 
   get length() {
@@ -24,7 +34,11 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    return this._length = length;
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new Error('Length must be a number');
+    }
   }
 
   get students() {
@@ -32,6 +46,10 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    return this._students = students;
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new Error('Students must be an array');
+    }
   }
 }
