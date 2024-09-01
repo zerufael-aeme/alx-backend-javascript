dd04fa9a1929c49536a90d679e53980db09c5764
zerufael <zerufaelaemero@gmail.com>
2024-09-01 13:03:54 -0700

    edit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (new.target === Building) {
+      throw new Error("Cannot instantiate an abstract class.");
+    }
   }
 
   get sqft() {
