5bcb0d3aba006375c3b101e17eaa7c223f7435af
zerufael <zerufaelaemero@gmail.com>
2024-09-01 07:20:58 -0700

    edit

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,5 @@
-import Currency from './3-currency';
-
 export default class Pricing {
-  constructor(amount, currency){
+  constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
