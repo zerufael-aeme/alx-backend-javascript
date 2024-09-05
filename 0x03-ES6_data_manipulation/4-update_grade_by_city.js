a4f62910d0969d8acca5e50121ced23f8bef368f
zerufael <zerufaelaemero@gmail.com>
2024-09-05 03:58:10 -0700

    edit

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,32 @@
-export default function updateStudentGradeByCity(listOfStudents, city, newGrades) {
-  const studentsByLocation = listOfStudents.filter((student) => student.location === city);
-  
-  const updatedStudents = studentsByLocation.map((student) => {
-    const gradeItem = newGrades.find((item) => item.studentId === student.id);
-  
-    return {
-      ...student,
-      grade: gradeUtem ? gradeItem.grade : 'N/A'
-    };
-  });
-  
-  return updatedStudents;
+/**
+ * Updates the grades of a list of students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {*} city - The city of students.
+ * @param {{
+ *   studentId: Number,
+ *   grade: Number,
+ * }[]} newGrades - The new grades to be given to a student.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
+
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
