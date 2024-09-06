8671a62861794bde2960624d336065485602ea98
zerufael <zerufaelaemero@gmail.com>
2024-09-06 08:29:09 -0700

    edit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString = '') {
     let cleanString = '';
 
+    if(!startString) {
+        return startString;
+    }
+
     set.forEach((item) => {
         if (item && item.startsWith(startString)) {
             cleanString += `${item.slice(startString.length)}-`;
