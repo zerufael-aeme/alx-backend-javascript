7fe5198d6038ae8f9224f4011c8ef61c9340751b
zerufael <zerufaelaemero@gmail.com>
2024-09-06 07:41:47 -0700

    edit

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -6,7 +6,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-    // Set the value at the specified position using DataView
+  // Set the value at the specified position using DataView
   view.setInt8(position, value);
 
   return view;
