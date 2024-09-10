2b992b08d5ca3b21a2185dda215af88a6ec53bd1
zerufael <zerufaelaemero@gmail.com>
2024-09-10 05:55:09 -0700

    edit

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -14,7 +14,6 @@ const MAX_ENDPOINT_CALLS = 5;
  *   protocol: String,
  *   name: String,
  * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
